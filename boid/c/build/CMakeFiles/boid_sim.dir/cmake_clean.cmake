29cff83cf0c27a86ad353ee2947e41ec31bd621f
Albin <a.folketoft@gmail.com>
2025-05-17 10:41:17 +0200

    boids c working as intended

---
+++ boid/c/build/CMakeFiles/boid_sim.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boid_sim.dir/link.d"
+  "CMakeFiles/boid_sim.dir/main.c.o"
+  "CMakeFiles/boid_sim.dir/main.c.o.d"
+  "boid_sim"
+  "boid_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/boid_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
